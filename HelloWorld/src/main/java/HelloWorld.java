d8791c8adf329fa5c8ce6136a109f56c62cdf4fe
tomburke <tomburke@localhost>
2024-03-16 14:33:37 -0400

    Initial Commit

---
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
